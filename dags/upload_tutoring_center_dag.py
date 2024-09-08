617fa445bd3e7e095cf88592039735b22a72b2aa
Jinsu Kim <jinsukim@Jinsus-MacBook-Air.local>
2024-09-09 00:01:22 +0900

    modify upload tutoring center

--- dags/upload_tutoring_center_dag.py
+++ dags/upload_tutoring_center_dag.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 from duckdb_provider.hooks.duckdb_hook import DuckDBHook
 from pendulum import datetime
 
@@ -21,9 +22,29 @@ select
     code
 from common_tutoring_center_code
         """
-        df: pd.Dataframe = conn.execute(sql).df()
-        return df.to_numpy().tolist()
+        df = conn.execute(sql).df()
+        return df.to_numpy().flatten().tolist()
+
+    @task
+    def get_df(code):
+        url = "https://open.neis.go.kr/hub/acaInsTiInfo"
+        params = {
+            "KEY": "6f05968fce924b00aa9253e3e95f3bf9",
+            "Type": "json",
+            "pIndex": 1,
+            "pSize": 1000,
+            "ATPT_OFCDC_SC_CODE": code,
+        }
+        raw_response = requests.get(
+            url,
+            params=params,
+        )
+        response = raw_response.json()["acaInsTiInfo"]["row"]
+        df = pd.DataFrame(response)
+        return df
 
     codes = get_tutoring_center_code()
+    get_df.expand(code=codes)
+
 
 upload_tutoring_center_dag()
